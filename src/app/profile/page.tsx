7d078cea4f9dba28ef2a8a91c8a4243acdbf3f7d
DimonBel <dimabelih2004pro3@gmail.com>
2025-09-12 12:23:50 +0300

    Add the Profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,44 @@
+import { ChartLineMultiple } from "@/features/Profile/chart-line-multiple";
+import PostsContainer from "@/features/postsContainer/PostsContainer";
+import Image from "next/image";
+
 export default function Profile() {
-  return <div></div>;
+  return (
+    <div className='flex min-h-screen bg-[#F3F4F6]'>
+      
+      <main className='flex-1 p-6'>
+        <div className='grid grid-cols-1 gap-6 lg:grid-cols-3'>
+          <section className='col-span-2 rounded-xl border bg-white p-8 shadow-sm'>
+            <div className='flex flex-col items-center gap-4'>
+              <Image
+                src='/window.svg'
+                alt='Profile avatar'
+                width={176}
+                height={176}
+                className='h-44 w-44 rounded-full object-cover'
+              />
+              <div className='text-center'>
+                <h1 className='text-2xl font-semibold'>Macho Man - you are alive!</h1>
+                <div className='mt-2 space-y-1 text-[#818181]'>
+                  <p>Joined us: 24 july 2021</p>
+                  <p>Total posts: 2024</p>
+                  <p>Total comments: 1024</p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section className='col-span-1'>
+            <ChartLineMultiple />
+          </section>
+        </div>
+
+        <h2 className='mt-8 text-center text-lg font-semibold'>Your Posts</h2>
+
+        <section className='scrollbar-hide mt-4 max-h-[80vh] overflow-y-auto rounded-lg bg-white p-4 shadow-sm'>
+          <PostsContainer />
+        </section>
+      </main>
+    </div>
+  );
 }
